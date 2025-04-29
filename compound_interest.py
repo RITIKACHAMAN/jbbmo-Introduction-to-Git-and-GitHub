9e4fd62615338eb369a18cfbc50188162c6fe91b
RITIKACHAMAN <145255150+RITIKACHAMAN@users.noreply.github.com>
2025-04-29 23:23:54 +0530

    Update compound_interest.py

--- compound_interest.py
+++ compound_interest.py
@@ -1,3 +1,4 @@
+# a little typo fixing for project
 # This script calculates yearly compound interest given principal, annual rate of interest and time period in years.
 # Do not use this in production. Sample purpose only.
 
